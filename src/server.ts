c7870d10659e664f09d94c5ecbfbdea4267e05d9
KISMAT1234 <kismatbohora123@gmail.com>
2024-10-02 08:57:51 +0545

    server runned and database successfull of connection

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+import sequelize from './Database/sequelizeConnection'
+
+const app = express();
+
+sequelize
+  .authenticate()
+  .then(() => {
+    console.log('Database connection successful!!');
+  })
+  .catch((err) => {
+    console.error('Unable to connect to the database:', err);
+  });
+
+app.listen(8000, () => {
+  console.log(`Server is running on port 8000`);
+});
