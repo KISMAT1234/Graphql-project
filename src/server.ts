f5acd9e4a3ceaee0893ae0d4fc6b9b65d6d69b26
KISMAT1234 <kismatbohora123@gmail.com>
2024-10-04 08:38:07 +0545

    graphql server runned successfully

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,42 @@
-import express from 'express';
-import sequelize from './Database/sequelizeConnection'
+import { ApolloServer, gql } from 'apollo-server';
+import sequelize from './Database/sequelizeConnection';
+import dotenv from 'dotenv';
 
-const app = express();
+dotenv.config();
+
+const PORT = process.env.PORT || 8000;
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+    greet(name: String!): String
+  }
+`;
+
+// Step 3: Define resolvers
+const resolvers = {
+  Query: {
+    hello: () => 'Hello, world!',
+    greet: (_: any, args: { name: string }) => `Hello, ${args.name}!`,
+  },
+};
+
+async function startApolloServer() {
+  const server = new ApolloServer({
+    // typeDefs,
+    // resolvers: [userResolver, postResolver, commentResolver, replyResolver],
+    // context: ({ req }) => {
+    //   return { user: req.user };
+    // },
+    typeDefs,
+    resolvers,
+  });
+
+  const { url } = await server.listen(PORT);
+  console.log(`Server is running at ${url}`);
+}
+
+startApolloServer();
 
 sequelize
   .authenticate()
@@ -12,7 +47,5 @@ sequelize
     console.error('Unable to connect to the database:', err);
   });
 
-  console
-app.listen(8000, () => {
-  console.log(`Server is running on port 8000`);
-});
+
+
