c7f019029735610fe643e80e0fbd2d6e81bcaebe
KISMAT1234 <kismatbohora123@gmail.com>
2024-10-05 10:28:31 +0545

    configured setup of resolvers and typedefs

--- src/server.ts
+++ src/server.ts
@@ -1,35 +1,22 @@
 import { ApolloServer, gql } from 'apollo-server';
 import sequelize from './Database/sequelizeConnection';
 import dotenv from 'dotenv';
+import { userResolver } from './GraphQL/resolvers/index';
+import typeDefs from './GraphQL/typeDefs/index';
 
 dotenv.config();
 
 const PORT = process.env.PORT || 8000;
 
-const typeDefs = gql`
-  type Query {
-    hello: String
-    greet(name: String!): String
-  }
-`;
-
-// Step 3: Define resolvers
-const resolvers = {
-  Query: {
-    hello: () => 'Hello, world!',
-    greet: (_: any, args: { name: string }) => `Hello, ${args.name}!`,
-  },
-};
 
 async function startApolloServer() {
   const server = new ApolloServer({
-    // typeDefs,
-    // resolvers: [userResolver, postResolver, commentResolver, replyResolver],
+    typeDefs,
+    resolvers: [userResolver],
     // context: ({ req }) => {
     //   return { user: req.user };
     // },
-    typeDefs,
-    resolvers,
+
   });
 
   const { url } = await server.listen(PORT);
