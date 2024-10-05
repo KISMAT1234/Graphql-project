8e4ac636cce1f3780f461a7b7c4dfb4f38f07538
KISMAT1234 <kismatbohora123@gmail.com>
2024-10-05 10:29:15 +0545

    configured schema typedefs

--- src/GraphQL/typeDefs/userTypeDefs.ts
+++ src/GraphQL/typeDefs/userTypeDefs.ts
@@ -1,14 +1,9 @@
 import { gql } from 'apollo-server-express';
 
 export const userType = gql`
-type User {
-    id: ID!
-    username: String!
-    email: String!
-    password: String!
-    posts: [Post!]!
+type Query {
+  user: String
 }
-
 
 `
 
