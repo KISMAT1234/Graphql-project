c9702c8e7bb90835b00b450ed1631afe36c5e427
KISMAT1234 <kismatbohora123@gmail.com>
2024-12-03 18:07:38 +0545

    user type rechange

--- src/GraphQL/typeDefs/userTypeDefs.ts
+++ src/GraphQL/typeDefs/userTypeDefs.ts
@@ -1,8 +1,26 @@
 import { gql } from 'apollo-server-express';
 
 export const userType = gql`
+
+type User {
+    id: ID!
+    name: String!
+    email: String!
+}
+
+input InputUser{
+    name: String!
+    email: String!
+    password: String!
+}
+
 type Query {
-  user: String
+  getUser(id: ID!): User!
+  getAllUser: [User!]!
+}
+
+type Mutation {
+  registerUser(registerData: InputUser!): User
 }
 
 `
