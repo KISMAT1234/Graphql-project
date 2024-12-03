a575f37bc1d63c74cfd767a1c373e05b6bda1b25
KISMAT1234 <kismatbohora123@gmail.com>
2024-12-03 18:08:04 +0545

    auhtentication added

--- src/server.ts
+++ src/server.ts
@@ -3,6 +3,7 @@ import sequelize from './Database/sequelizeConnection';
 import dotenv from 'dotenv';
 import { userResolver } from './GraphQL/resolvers/index';
 import typeDefs from './GraphQL/typeDefs/index';
+import { authenticate } from './Middleware/authentication.Middleware';
 
 dotenv.config();
 
@@ -13,8 +14,24 @@ async function startApolloServer() {
   const server = new ApolloServer({
     typeDefs,
     resolvers: [userResolver],
-    // context: ({ req }) => {
-    //   return { user: req.user };
+    // context: ({ req, res }) => {
+    //   console.log('inside context')
+    //   const authHeader = req.headers.authorization
+  
+    //   console.log(authHeader,'header getting')
+    //   if (authHeader) {
+    //     const token = authHeader?.split(' ')[1]
+    //     console.log(token,'token after split')
+
+    //     if (!token) {
+    //       return { user: null };
+    //     }
+
+    //     const decodedToken = authenticate(token)
+    //     return { user: decodedToken };
+    //   }
+    //   return {user: null}
+
     // },
 
   });
