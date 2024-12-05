48698a472d7516508932246b053aab4537a46cf3
KISMAT1234 <kismatbohora123@gmail.com>
2024-12-05 08:11:02 +0545

    created user service of crud operation in databases

---
+++ src/Service/UserService.ts
@@ -0,0 +1,33 @@
+import { UserInputError } from "apollo-server-express";
+import User from "../Models/User";
+
+export class UserService {
+
+    static async registerUser(data:any){
+         
+    }
+    static async getUserById(id:number) {
+        try{
+
+            const oneUserData = await User.findByPk(id);
+            console.log(oneUserData,'one user data')
+            return oneUserData;
+        }catch (error){
+            console.error(error,'error in fetching user id ');
+            throw new UserInputError('Error fetching user by ID');
+        }
+    }
+
+    static async getAllUserData() {
+        try{
+            console.log("Inside all data")
+            const oneUserData = await User.findAll();
+            console.log(oneUserData,'all user data')
+            return oneUserData;
+        }catch (error){
+            console.error(error,'error in fetching user id ');
+            throw new UserInputError('Error fetching user by ID');
+        }
+    }
+
+}
