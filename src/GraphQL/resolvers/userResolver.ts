5a68c7baf93a82b94421bb484a092d40fb00af12
KISMAT1234 <kismatbohora123@gmail.com>
2024-12-05 08:11:18 +0545

    fetched user data in resolvers

--- src/GraphQL/resolvers/userResolver.ts
+++ src/GraphQL/resolvers/userResolver.ts
@@ -1,10 +1,21 @@
+import { UserService } from "../../Service/UserService";
+
 const userResolver = {
     Query: {
-      user: () => {
-        // Your logic for fetching user here
-        return'kismat'
+      getUser: async(_:any, __:any, id:any):Promise<any> => {
+        return await UserService.getUserById(id);
+      },
+
+      getAllUser: async(_:any, __:any, id:any):Promise<any> => {
+        return await UserService.getAllUserData();
       },
     },
-  };
 
+    Mutation: {
+      registerUser: async (_:any,__:any, { registerData }:any) => {
+        console.log(registerData,'registration data')
+         return await UserService.registerUser(registerData)
+      }
+  }
+}
 export default userResolver
