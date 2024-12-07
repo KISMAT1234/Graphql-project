ee03c4d82f06cbd6aa0f54b052205227fe6ab9f5
KISMAT1234 <kismatbohora123@gmail.com>
2024-12-07 13:56:09 +0545

    services updated in login

--- src/Service/UserService.ts
+++ src/Service/UserService.ts
@@ -3,7 +3,7 @@ import User from "../Models/User";
 
 export class UserService {
 
-    static async registerUser(data:any){
+    static async registerUserService(data:any){
         console.log(data,'data here')
         const newUser = await User.create(data);
         console.log(newUser,'after save')
@@ -29,8 +29,38 @@ export class UserService {
             console.log(oneUserData,'all user data')
             return oneUserData;
         }catch (error){
-            console.error(error,'error in fetching user id ');
+            console.error(error,'error in signup');
             throw new UserInputError('Error fetching user by ID');
+        }
+    }
+
+    static async logInUserService(logInDetails:any) {
+        try{
+            // console.log(logInDetails.LoginData.password)
+            const { email, password } = logInDetails.LoginData
+            console.log(email,'email in resolvers')
+            const oneUserData = await User.findOne({
+                where:{
+                    email:email,
+                    password:password
+                }
+            });
+            console.log(oneUserData,'one user data')
+            if (oneUserData) {
+                return {
+                    message: 'Login successful',
+                    userDetails: oneUserData                };
+            } else {
+                throw new UserInputError('Please provide correct username and password', {
+                    invalidArgs: ['email', 'password'], // Optionally add extra info to help with debugging
+                });
+            }
+        }catch(error){
+            console.error(error,'error in signup ');
+            if (error instanceof UserInputError) {
+                throw error; 
+            }
+            throw new UserInputError('Error in server');
         }
     }
 
