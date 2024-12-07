ac6c69d0dd364f44759f1eb65abc6cc30487d85a
KISMAT1234 <kismatbohora123@gmail.com>
2024-12-07 13:55:10 +0545

    login done successfully

--- src/GraphQL/resolvers/userResolver.ts
+++ src/GraphQL/resolvers/userResolver.ts
@@ -10,16 +10,19 @@ const userResolver = {
         return await UserService.getAllUserData();
       },
 
-      logIn: async(_:any, __:any, id:any):Promise<any> => {
-        return await UserService.getUserById(id);
-      }
+      
     },
 
     Mutation: {
       signUp: async (_:any, { registerData }:any) => {
         console.log(registerData,'registration data')
         console.log(registerData.email,'email')
-        return await UserService.registerUser(registerData)
+        return await UserService.registerUserService(registerData)
+      },
+
+      logIn: async(_:any, logInDetails:any):Promise<any> => {
+        console.log(logInDetails,'login  details in resolvers')
+        return await UserService.logInUserService(logInDetails);
       }
 
    }
