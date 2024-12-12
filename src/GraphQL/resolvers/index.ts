b756ec213655945716df6b8326db752c04fcb4d4
KISMAT1234 <kismatbohora123@gmail.com>
2024-12-12 20:46:57 +0545

    doctor type def and resolver included

--- src/GraphQL/resolvers/index.ts
+++ src/GraphQL/resolvers/index.ts
@@ -1,3 +1,4 @@
 import userResolver from "./userResolver";
+import doctorResolver from "./doctorResolver";
 
-export {userResolver}
+export {userResolver, doctorResolver} 
--- src/GraphQL/typeDefs/index.ts
+++ src/GraphQL/typeDefs/index.ts
@@ -1,7 +1,9 @@
 import { gql } from 'apollo-server-express';
 import { userType } from './userTypeDefs';
+import { doctorType } from './doctorTypeDef';
 
 const typeDefs = gql`
 ${userType}
+${doctorType}
 `
 export default typeDefs
